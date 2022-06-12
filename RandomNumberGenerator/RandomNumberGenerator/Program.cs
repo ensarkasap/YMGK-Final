c5ffa715ee66f81cd7daa269b0be6e75e85ab12c
ensarkasap <59575518+ensarkasap@users.noreply.github.com>
2022-06-12 23:18:40 +0300

    Initial commit

---
+++ RandomNumberGenerator/RandomNumberGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static StreamWriter Yaz = null;
+        static void Main(string[] args)
+        {            
+            try
+            {
+                Yaz = new StreamWriter(@"C:\Users\ensar\Desktop\RandomNumberGenerator\Data8.txt");
+                for (int i = 0; i < 1000000; i++)
+                {
+                   Yaz.Write(RandomNumberGenerator.Instance.Generate(0, 1) + "\r\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+            finally
+            {
+                Yaz.Flush();
+                Yaz.Close();
+            }                                 
+        }
+    }
+}
